bf32e82977e2df4760d23969a7bef08011dfefc9
Archan Banerjee <archanbanerjee70@gmail.com>
2022-01-02 12:59:41 +0530

    added payment routes

--- controllers/user/private.js
+++ controllers/user/private.js
@@ -1,147 +1,117 @@
-const User=require('../../models/User');
-const Artist=require('../../models/Artist');
-const Service=require('../../models/Service');
-const Razorpay=require('razorpay');
+const User = require('../../models/User');
+const Artist = require('../../models/Artist');
+const Service = require('../../models/Service');
+const Razorpay = require('razorpay');
 const request = require('request');
-const {nanoid}=require('nanoid');
+const { nanoid } = require('nanoid');
 
 const razorInstance = new Razorpay({
-    key_id : process.env.KEY_ID,
-    key_secret : process.env.KEY_SECRET
-  })
-
-// Rechage wallet
-exports.rechargeWallet=async(req,res)=>{
-    try {
-        let {amount}=req.body;
-        amount=parseFloat(amount);
-        console.log(amount);
-        var instance = new Razorpay({
-            key_id: process.env.KEY_ID,
-            key_secret: process.env.KEY_SECRET,
-          })
-          var options = {
-            amount: amount * 100, // amount in the smallest currency unit
-            currency: 'INR',
-            receipt: nanoid(),
-          }
-          instance.orders.create(options, async (err, order) =>{
-            if (!err) {
-                let uBalance=parseFloat(req.user.balance);
-        uBalance=uBalance+amount;
-        uBalance=uBalance.toString();
-        await User.updateOne({_id:req.user._id},{
-            $set:{
-                balance:uBalance
-            }
-        })
-            //   res
-                // .status(200)
-                // .json({ ...order, key: process.env.KEY_ID })
-                res.status(200).json({message:"Recharge successful!"});
-            } else {
-              console.log(err)
-              res.status(500).json({
-                message: 'There was some error, please contact your administrator',
-              })
-            }
-          })
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"Something went wrong!"});
-    }
-}
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET
+})
 
 //Order
-exports.order=async(req,res)=>{
-    try {
-        const options ={
-            amount : 10*100,
-            currency : "INR",
-            receipt: nanoid(),
-            payment_capture: 0, //1
-      
-          };
-          razorInstance.orders.create(options,async function(err,order){
-            if(err){
-             res.status(500).json({
-                message: "Something error!s"
-              })
-            }
-            else res.status(200).json(order);
-          });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"Something went wrong!"});
-    }
+exports.order = async (req, res) => {
+  try {
+    const { amount } = req.params;
+    const options = {
+      amount: parseInt(amount) * 100,
+      currency: "INR",
+      receipt: nanoid(),
+      payment_capture: 0, //1
+
+    };
+    razorInstance.orders.create(options, async function (err, order) {
+      if (err) {
+        res.status(500).json({
+          error: "Something went wrong!"
+        })
+      }
+      else res.status(200).json(order);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong!" });
+  }
 }
 
 //Capture
-exports.capture=async(req,res)=>{
-    try {
-        return request(
-            {
-              method : "POST",
-              url : `https://${process.env.KEY_ID}:${process.env.KEY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
-              form:{
-                amount : 10 *100,
-                currency: "INR"
-              },
-            },
-            async function(err,response,body){
-              if(err){
-                res.status(500).json({
-                  message: "Something error!s"
-                })
-              }
-              else res.status(200).json(body)
+exports.capture = async (req, res) => {
+  try {
+    let { amount } = req.body;
+    amount=parseInt(amount);
+    return request(
+      {
+        method: "POST",
+        url: `https://${process.env.KEY_ID}:${process.env.KEY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
+        form: {
+          amount: amount * 100,
+          currency: "INR"
+        },
+      },
+      async function (err, response, body) {
+        if (err) {
+          res.status(500).json({
+            error: "Something went wrong!"
+          })
+        }
+        else {
+          let uBalance = parseFloat(req.user.balance);
+          uBalance = uBalance + amount;
+          uBalance = uBalance.toString();
+          await User.updateOne({ _id: req.user._id }, {
+            $set: {
+              balance: uBalance
             }
-          )
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"Something went wrong!"});
-    }
+          })
+          res.status(200).json(body)
+        }
+      }
+    )
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong!" });
+  }
 }
 
 //Buy service
-exports.buyServices=async(req,res)=>{
-    try {
-        const {serviceId}=req.body;
-        const service=await Service.findOne({_id:serviceId});
-        if(!service) res.status(400).json({error:"Service not found!"});
-        else{
-            let uBalance=parseFloat(req.user.balance);
-            let servicePrice=parseFloat(service.amount);
-            if(uBalance>=servicePrice){
-                await Service.updateOne({_id:serviceId},{
-                    $push:{
-                        users:req.user._id
-                    }
-                })
-                const artist=await Artist.findOne({_id:service.createdBy});
-                let aBalance=parseFloat(artist.balance);
-                aBalance=aBalance+(servicePrice*70.00/100.00);
-                aBalance=aBalance.toString();
-                await Artist.updateOne({_id:artist._id},{
-                    $set:{
-                        balance:aBalance
-                    }
-                })
-                uBalance=uBalance-servicePrice;
-                uBalance=uBalance.toString();
-                await User.updateOne({_id:req.user._id},{
-                    $set:{
-                        balance:uBalance
-                    }
-                })
-                res.status(200).json({message:"Service added!"});
-            }
-            else res.status(400).json({error:"User doesn't have enough balance!"});
-    
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"Something went wrong!"});
+exports.buyServices = async (req, res) => {
+  try {
+    const { serviceId } = req.body;
+    const service = await Service.findOne({ _id: serviceId });
+    if (!service) res.status(400).json({ error: "Service not found!" });
+    else {
+      let uBalance = parseFloat(req.user.balance);
+      let servicePrice = parseFloat(service.amount);
+      if (uBalance >= servicePrice) {
+        await Service.updateOne({ _id: serviceId }, {
+          $push: {
+            users: req.user._id
+          }
+        })
+        const artist = await Artist.findOne({ _id: service.createdBy });
+        let aBalance = parseFloat(artist.balance);
+        aBalance = aBalance + (servicePrice * 70.00 / 100.00);
+        aBalance = aBalance.toString();
+        await Artist.updateOne({ _id: artist._id }, {
+          $set: {
+            balance: aBalance
+          }
+        })
+        uBalance = uBalance - servicePrice;
+        uBalance = uBalance.toString();
+        await User.updateOne({ _id: req.user._id }, {
+          $set: {
+            balance: uBalance
+          }
+        })
+        res.status(200).json({ message: "Service added!" });
+      }
+      else res.status(400).json({ error: "User doesn't have enough balance!" });
+
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong!" });
+  }
 }
--- routes/user/private.js
+++ routes/user/private.js
@@ -1,5 +1,5 @@
 const router=require('express').Router();
-const {buyServices,rechargeWallet,order,capture}=require('../../controllers/user/private');
+const {buyServices,order,capture}=require('../../controllers/user/private');
 const {protectUser}=require('../../middlewares/protect');
 
 //Buy service
@@ -10,10 +10,20 @@ const {protectUser}=require('../../middlewares/protect');
 //Token : Yes
 router.put('/buyservice',protectUser,buyServices);
 
-router.put('/rechargewallet',protectUser,rechargeWallet);
+//Recharge wallet (order)
+//Route : '/api/user/private/order/:amount'
+//Method : GET
+//Body : N/A
+//Params : {amount}
+//Token : Yes
+router.get('/order/:amount',protectUser,order);
 
-router.get('/order',order);
-
-router.post('/capture/:paymentId',capture);
+//Recharge wallet (capture)
+//Route : '/api/user/private/capture/:paymentId'
+//Method : POST
+//Body : {amount}
+//Params : {paymentId}
+//Token : Yes
+router.post('/capture/:paymentId',protectUser,capture);
 
 module.exports=router;
