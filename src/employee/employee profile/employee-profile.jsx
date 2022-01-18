3d51799fd75801fac4a7ea7853b076fc8b95edb9
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-01-18 21:00:57 +0530

    employee profile and edit profile

--- src/employee/employee profile/employee-profile.jsx
+++ src/employee/employee profile/employee-profile.jsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import  Logo from '../../assets/Ellipse 58.png';
-import './employee-profile.css'
-const EmployeeProfile=()=>{
-    return(
-        <div className='employee-profile'>
-          <div className='img-cont-inc' style={{paddingTop:"1rem"}}>
-         <span id='fanstar'>Fanstar logo</span>
-       
-        </div>
-        <img id='logo-img' src={Logo}/>
-        <div className='upper-container'>
-       
-        </div>
-        <div className='main-container'>
+import React, { Fragment, useState } from 'react';
+import EmployeeEditProfile from './EmployeeEditProfile';
+import Logo from '../../assets/Ellipse 58.png';
+import { ReactComponent as Home } from '../.././assets/home.svg';
+import { ReactComponent as Chat } from '../.././assets/chat.svg';
+import { ReactComponent as Profile } from '../.././assets/employeeprofile.svg';
+import demoProfile from '../../assets/demoProfile.png';
+import editIcon from '../../assets/editIcon.png';
+import './employee-profile.css';
 
-        </div>
+const initialData = {
+  name: 'Shikhar rastogi',
+  gender: 'Male',
+  email: 'shikhar@gmail.com',
+  phone: '7894561203',
+};
 
+const EmployeeProfile = () => {
+  const [profileInfo, setProfileInfo] = useState(initialData);
+  const [isEdit, setIsEdit] = useState(false);
+  return (
+    <div className='employee-profile'>
+      {!isEdit ? (
+        <Fragment>
+          <div className='img-cont-inc' style={{ paddingTop: '1rem' }}>
+            <span id='fanstar'>Fanstar logo</span>
+          </div>
+          <img id='logo-img' src={Logo} />
+          <div className='profile-main-container'>
+            <div className='profileImage-section'>
+              <div className='profileImg-div'>
+                <img src={demoProfile} alt='profile' className='profileImg' />
+              </div>
+              <div className='editBtnDiv' onClick={() => setIsEdit(true)}>
+                <span className='editText'>Edit</span>
+                <img src={editIcon} alt='edit' className='editIcon' />
+              </div>
+            </div>
+            <div className='profileInfo-section'>
+              <div className='inputField-div'>
+                <label className='inputField-label'>Your full name</label>
+                <input
+                  type='text'
+                  value={profileInfo.name}
+                  className='inputField'
+                  disabled
+                />
+              </div>
+              <div className='inputField-div'>
+                <label className='inputField-label'>Your Gender</label>
+                <input
+                  type='text'
+                  value={profileInfo.gender}
+                  className='inputField'
+                  disabled
+                />
+              </div>
+              <div className='inputField-div'>
+                <label className='inputField-label'>Your E-mail</label>
+                <input
+                  type='text'
+                  value={profileInfo.email}
+                  className='inputField'
+                  disabled
+                />
+              </div>
+              <div className='inputField-div'>
+                <label className='inputField-label'>Your phone number</label>
+                <input
+                  type='text'
+                  value={profileInfo.phone}
+                  className='inputField'
+                  disabled
+                />
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      ) : (
+        <EmployeeEditProfile profileInfo={profileInfo} setIsEdit={setIsEdit} />
+      )}
+      <div className='icons-tab'>
+        <div className='nav'>
+          <Home />
+
+          <Chat />
+
+          <Profile />
         </div>
-    )
-    
-    
-}
+      </div>
+    </div>
+  );
+};
 export default EmployeeProfile;
--- src/user/susbscriptions/subscriptions.jsx
+++ src/user/susbscriptions/subscriptions.jsx
@@ -3,28 +3,32 @@ import { Link } from 'react-router-dom';
 import Ban from '../.././assets/register-banner.png';
 import Back from '../.././assets/Rectangle 736.png';
 import './subscriptions.css';
-const Subscription=()=>{
-     
-    return(
-        <div className='subscription-main'>
-            <img  className='back-img' src={Back} alt="background-img"/>
-            <div className='subscription-container'>
-                <h1 className="sub-head">Subscriptions</h1>
-                <div className='subscription-main-2'>
-                    <div className='img-container'>  <img  id="ban-img" src={Ban} alt="banner-img"/>  </div>
-                
-                <h1 id="sub-main-con">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendre</h1>
-                <select name="plans" className="choose-plan">
-                <option value="1month">1 month membership @ 599/-</option>
-                <option value="plan2">Plan 2</option>
-                <option value="plan3">Plan 3</option>
-                </select>
-                <Link to="/subscribe" style={{textDecoration:"none"}}><button className='btn-sub'>Subscribe now</button></Link>   
-                </div>
+const Subscription = () => {
+  return (
+    <div className='subscription-main'>
+      <img className='back-img' src={Back} alt='background-img' />
+      <div className='subscription-container'>
+        <h1 className='sub-head'>Subscriptions</h1>
+        <div className='subscription-main-2'>
+          <div className='img-container'>
+            {' '}
+            <img id='ban-img' src={Ban} alt='banner-img' />{' '}
+          </div>
 
-            </div>
-
+          <h1 id='sub-main-con'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendre
+          </h1>
+          <select name='plans' className='choose-plan'>
+            <option value='1month'>1 month membership @ 599/-</option>
+            <option value='plan2'>Plan 2</option>
+            <option value='plan3'>Plan 3</option>
+          </select>
+          <Link to='/subscribe' style={{ textDecoration: 'none' }}>
+            <button className='btn-sub'>Subscribe now</button>
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default Subscription;
