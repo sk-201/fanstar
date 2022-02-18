b25c2628df903cd9e1d6151c2cfc50993c33d947
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-02-19 02:05:29 +0530

    add to home for artist

--- src/App.js
+++ src/App.js
@@ -61,7 +61,6 @@ function App() {
 
     return (
       <div className='App'>
-        <Add
         <Routes>
           <Route path='/' exact element={<IntroScreen />} />
           <Route path='/login' exact element={<Login />} />
