a8c25b48dee95cce85d2a610b7cc5bc1a251a1fc
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-02-19 01:18:40 +0530

    changes for add to home screen

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  
   </React.StrictMode>,
   document.getElementById('root')
 );
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
