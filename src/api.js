355e70f7151ef124aac294b0f70315e4ce47e90e
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-02-22 21:36:51 +0530

    change baseurl

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: 'https://fanstar-backend.herokuapp.com',
+  baseURL: 'https://fanstar-backend-uiwtg.ondigitalocean.app/',
 });
 export default API;
