ef7e024a8a0e5e6eed4e823bf915c617d2ce54f5
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-03-09 15:37:10 +0530

    testing

--- src/artist/artist-landing/artist-landing.jsx
+++ src/artist/artist-landing/artist-landing.jsx
@@ -36,18 +36,24 @@ const ArtistLanding = () => {
 
   const handleInstall = (e) => {
     // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
+    // btnAdd.style.display = 'none';
     // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
+
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      console.log(deferredPrompt);
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    } else {
+      console.log('Not available');
+    }
   };
 
   // Check if the app is succesfully installed
